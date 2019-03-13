fb1e306d4efae1edafe81e0010a2bb928a027c1c
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-03-13 20:44:42 +0500

    docs(storybook): added storybook

--- src/JsonLayout/__stories__/JsonSchema.stories.tsx
+++ src/JsonLayout/__stories__/JsonSchema.stories.tsx
@@ -1,17 +1,22 @@
-import { JsonSchema } from '../../../getComponent';
+import { JsonLayoutProps } from '..';
 import React from 'react';
+import { getComponent } from '@bluebase/components';
 import storiesOf from '@bluebase/storybook-addon';
+
+const JsonLayout = getComponent<JsonLayoutProps>('JsonLayout');
 
 storiesOf('JsonSchema', module)
 
 	.add('With default props', () => (
-		<JsonSchema schema={{
-			component: 'Text',
-			props: {
-				style: {
-					color: 'red'
-				}
-			},
-			text: 'This componenet is generated through JsonSchema Component',
-		}} />
+		<JsonLayout
+			schema={{
+				component: 'Text',
+				props: {
+					style: {
+						color: 'red'
+					}
+				},
+				text: 'This componenet is generated through JsonSchema Component',
+			}}
+		/>
 	));
