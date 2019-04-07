18bfd7c11a7d02cd2443a0c640226d378da05cfe
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-07 14:37:29 +0500

    test(FormCheckboxInput): Added tests

--- src/JsonForm/Fields/FormCheckboxInput.tsx
+++ src/JsonForm/Fields/FormCheckboxInput.tsx
@@ -16,15 +16,13 @@ export const FormCheckboxInput = (props: FormCheckboxInputProps) => {
 		<Field {...props}>
 		{({ field, form }: any) => {
 
-			const name = props.name || '';
+			const name = props.name;
 			const value = getIn(form.values, name);
 
 			const inputProps = {
 				...props,
-				// ...field,
 				checked: Boolean(value),
 				onValueChange: (_value: any, checked: boolean) => form.setFieldValue(field.name, checked),
-				// value,
 			};
 
 			return (<BaseFormField MainComponent={Checkbox} {...inputProps} />);
---
+++ src/JsonForm/Fields/__tests__/FormCheckboxInput.test.tsx
@@ -0,0 +1,101 @@
+import { BlueBaseApp } from '@bluebase/core';
+import { FormCheckboxInput } from '../FormCheckboxInput';
+import { Formik } from 'formik';
+import Plugin from '../../../index';
+import React from 'react';
+// import ReactNativePaperPlugin from '@bluebase/plugin-react-native-paper';
+import MaterialUIPlugin from '@bluebase/plugin-material-ui';
+import { mount } from 'enzyme';
+import { waitForElement } from 'enzyme-async-helpers';
+
+const fieldProps = {
+	label: 'Auto Login',
+	name: 'auto-login',
+	type: 'checkbox',
+};
+
+// const initialValues = {
+// 	'auto-login': true,
+// };
+
+
+describe('FormCheckboxInput', () => {
+
+
+	it('should a checkbox with value true', async () => {
+
+		const onSubmit = jest.fn();
+
+		const component = mount(
+			<BlueBaseApp plugins={[Plugin, MaterialUIPlugin]}>
+				<Formik initialValues={{ 'auto-login': true }} onSubmit={onSubmit}>
+					<FormCheckboxInput {...fieldProps as any} />
+				</Formik>
+			</BlueBaseApp>
+		);
+
+		await waitForElement(component as any, FormCheckboxInput);
+
+		// expect(component).toMatchSnapshot();
+
+		// Check fields
+		expect(component.find('Checkbox').last().prop('name')).toBe('auto-login');
+		expect(component.find('Checkbox').last().prop('checked')).toBe(true);
+	});
+
+
+	it('should a checkbox with value false', async () => {
+
+		const onSubmit = jest.fn();
+
+		const component = mount(
+			<BlueBaseApp plugins={[Plugin, MaterialUIPlugin]}>
+				<Formik initialValues={{ 'auto-login': false }} onSubmit={onSubmit}>
+					<FormCheckboxInput {...fieldProps as any} />
+				</Formik>
+			</BlueBaseApp>
+		);
+
+		await waitForElement(component as any, FormCheckboxInput);
+
+		// expect(component).toMatchSnapshot();
+
+		// Check fields
+		expect(component.find('Checkbox').last().prop('name')).toBe('auto-login');
+		expect(component.find('Checkbox').last().prop('checked')).toBe(false);
+	});
+
+
+	it('should update checkbox value from onValueChange callback', async () => {
+
+		const onSubmit = jest.fn();
+
+		const component = mount(
+			<BlueBaseApp plugins={[Plugin, MaterialUIPlugin]}>
+				<Formik initialValues={{ 'auto-login': false }} onSubmit={onSubmit}>
+					<FormCheckboxInput {...fieldProps as any} />
+				</Formik>
+			</BlueBaseApp>
+		);
+
+		await waitForElement(component as any, FormCheckboxInput);
+
+		// Initial state should be false
+
+		// Check fields
+		// expect(component).toMatchSnapshot();
+		expect(component.find('Checkbox').first().prop('name')).toBe('auto-login');
+		expect(component.find('Checkbox').first().prop('checked')).toBe(false);
+
+		// Update State
+		const onValueChange: any = component.find('Checkbox').first().prop('onValueChange');
+
+		onValueChange(null, true);
+		component.update();
+		// expect(component).toMatchSnapshot();
+
+		// New state should be true
+		expect(component.find('Checkbox').first().prop('name')).toBe('auto-login');
+		expect(component.find('Checkbox').first().prop('checked')).toBe(true);
+	});
+});
