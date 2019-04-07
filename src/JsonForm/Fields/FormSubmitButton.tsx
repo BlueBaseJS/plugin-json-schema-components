c0632595ebef710e5d937d15fd64cd910616671d
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-07 15:09:45 +0500

    test(Buttons): Added tests for submit and reset buttons

--- src/JsonForm/Fields/FormSubmitButton.tsx
+++ src/JsonForm/Fields/FormSubmitButton.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, Button, ButtonProps } from '@bluebase/components';
+import { Button, ButtonProps } from '@bluebase/components';
 import { FormikContext, connect } from 'formik';
 import React from 'react';
 
@@ -7,16 +7,12 @@ export interface FormSubmitButtonProps extends ButtonProps {
 	name: string,
 }
 
-// TODO: handle submitting state
 export const FormSubmitButton = connect((props: FormSubmitButtonProps & { formik: FormikContext<{}>; }) => {
 
 	const { handleSubmit, isSubmitting } = props.formik;
-	const children = isSubmitting ? <ActivityIndicator color="white" /> : props.children;
 
 	return (
-		<Button disabled={isSubmitting} {...props} onPress={handleSubmit} type="submit">
-		{children}
-		</Button>
+		<Button disabled={isSubmitting} loading={isSubmitting} {...props} onPress={handleSubmit} type="submit" />
 	);
 });
 
---
+++ src/JsonForm/Fields/__tests__/FormResetButton.test.tsx
@@ -0,0 +1,42 @@
+import { BlueBaseApp } from '@bluebase/core';
+import { FormResetButton } from '../FormResetButton';
+import { Formik } from 'formik';
+import MaterialUIPlugin from '@bluebase/plugin-material-ui';
+import Plugin from '../../../index';
+import React from 'react';
+import { mount } from 'enzyme';
+import { waitForElement } from 'enzyme-async-helpers';
+
+const fieldProps = {
+	name: 'reset',
+	type: 'reset',
+};
+
+const initialValues = {
+};
+
+
+describe('FormResetButton', () => {
+
+
+	it('should render all form fields in the schema', async () => {
+
+		const onSubmit = jest.fn();
+
+		const component = mount(
+			<BlueBaseApp plugins={[Plugin, MaterialUIPlugin]}>
+				<Formik initialValues={initialValues} onSubmit={onSubmit}>
+					<FormResetButton {...fieldProps as any} />
+				</Formik>
+			</BlueBaseApp>
+		);
+
+		await waitForElement(component as any, FormResetButton);
+
+		// expect(component).toMatchSnapshot();
+
+		// Check fields
+		expect(component.find('Button').first().prop('title')).toBe('Reset');
+		expect(component.find('Button').first().prop('type')).toBe('reset');
+	});
+});
