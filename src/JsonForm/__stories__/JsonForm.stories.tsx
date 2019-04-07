e20d0dfa33f1081cbd660e507cc9d0e2d5792bb3
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-07 20:57:20 +0500

    test(storybook-native): Setup a storybook native enviornment

--- src/JsonForm/__stories__/JsonForm.stories.tsx
+++ src/JsonForm/__stories__/JsonForm.stories.tsx
@@ -1,5 +1,6 @@
 import { JsonFormProps } from '..';
 import React from 'react';
+import { ScrollView } from 'react-native';
 import { getComponent } from '@bluebase/components';
 import storiesOf from '@bluebase/storybook-addon';
 
@@ -8,124 +9,126 @@ const JsonForm = getComponent<JsonFormProps>('JsonForm');
 storiesOf('JsonForm', module)
 
 	.add('With default props', () => (
-		<JsonForm
-			schema={{
-				fields: [
-					{
-						label: 'Username',
-						name: 'username',
-						required: true,
-						type: 'text',
-					},
-					{
-						schema: { component: 'Divider' },
-						type: 'component',
-					},
-					{
-						label: 'Password',
-						name: 'password',
-						type: 'password',
-					},
-					{
-						schema: { component: 'Divider' },
-						type: 'component',
-					},
-					{
-						label: 'Temp',
-						max: 32,
-						min: 16,
-						name: 'temp',
-						step: 1,
-						type: 'range',
-					},
-					{
-						schema: { component: 'Divider' },
-						type: 'component',
-					},
-					{
-						label: 'Language',
-						name: 'lang',
-						type: 'picker',
+		<ScrollView>
+			<JsonForm
+				schema={{
+					fields: [
+						{
+							label: 'Username',
+							name: 'username',
+							required: true,
+							type: 'text',
+						},
+						{
+							schema: { component: 'Divider' },
+							type: 'component',
+						},
+						{
+							label: 'Password',
+							name: 'password',
+							type: 'password',
+						},
+						{
+							schema: { component: 'Divider' },
+							type: 'component',
+						},
+						{
+							label: 'Temp',
+							max: 32,
+							min: 16,
+							name: 'temp',
+							step: 1,
+							type: 'range',
+						},
+						{
+							schema: { component: 'Divider' },
+							type: 'component',
+						},
+						{
+							label: 'Language',
+							name: 'lang',
+							type: 'picker',
 
-						items: [{
-							label: 'JavaScript',
-							value: 'js'
-						}, {
-							label: 'Java',
-							value: 'java'
-						}]
-					},
-					{
-						schema: { component: 'Divider' },
-						type: 'component',
-					},
-					{
-						label: 'Gender',
-						name: 'gender',
-						type: 'radio-group',
+							items: [{
+								label: 'JavaScript',
+								value: 'js'
+							}, {
+								label: 'Java',
+								value: 'java'
+							}]
+						},
+						{
+							schema: { component: 'Divider' },
+							type: 'component',
+						},
+						{
+							label: 'Gender',
+							name: 'gender',
+							type: 'radio-group',
 
-						items: [{
-							label: 'Male',
-							value: 'male'
-						}, {
-							label: 'Female',
-							value: 'female'
-						}]
-					},
-					{
-						schema: { component: 'Divider' },
-						type: 'component',
-					},
-					{
-						label: 'Remember Me',
-						name: 'remember',
-						type: 'checkbox',
-					},
-					{
-						schema: { component: 'Divider' },
-						type: 'component',
-					},
-					{
-						label: 'Auto Login',
-						name: 'auto-login',
-						type: 'switch',
-					},
-					{
-						schema: { component: 'Divider' },
-						type: 'component',
-					},
-					{
-						direction: 'right',
-						name: 'form-actions',
-						type: 'actions',
+							items: [{
+								label: 'Male',
+								value: 'male'
+							}, {
+								label: 'Female',
+								value: 'female'
+							}]
+						},
+						{
+							schema: { component: 'Divider' },
+							type: 'component',
+						},
+						{
+							label: 'Remember Me',
+							name: 'remember',
+							type: 'checkbox',
+						},
+						{
+							schema: { component: 'Divider' },
+							type: 'component',
+						},
+						{
+							label: 'Auto Login',
+							name: 'auto-login',
+							type: 'switch',
+						},
+						{
+							schema: { component: 'Divider' },
+							type: 'component',
+						},
+						{
+							direction: 'right',
+							name: 'form-actions',
+							type: 'actions',
 
-						fields: [{
-							name: 'reset',
-							type: 'reset',
-						}, {
-							name: 'submit',
-							title: 'Login',
-							type: 'submit',
-						}]
-					}
-				],
-				initialValues: {
-					'auto-login': true,
-					gender: 'female',
-					lang: 'js',
-					password: '',
-					remember: true,
-					temp: 26,
-					username: '',
-				},
-				onSubmit: (values, form) => {
-					// tslint:disable-next-line: no-console
-					console.log('Login form submitted by following values', values);
+							fields: [{
+								name: 'reset',
+								type: 'reset',
+							}, {
+								name: 'submit',
+								title: 'Login',
+								type: 'submit',
+							}]
+						}
+					],
+					initialValues: {
+						'auto-login': true,
+						gender: 'female',
+						lang: 'js',
+						password: '',
+						remember: true,
+						temp: 26,
+						username: '',
+					},
+					onSubmit: (values, form) => {
+						// tslint:disable-next-line: no-console
+						console.log('Login form submitted by following values', values);
 
-					setTimeout(() => {
-						form.setSubmitting(false);
-					}, 2000);
-				},
-			}}
-		/>
+						setTimeout(() => {
+							form.setSubmitting(false);
+						}, 2000);
+					},
+				}}
+			/>
+		</ScrollView>
 	));
