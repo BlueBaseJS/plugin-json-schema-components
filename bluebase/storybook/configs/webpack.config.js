cdd8a4afa913235e3b2feb4993cb522d5f307d5a
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-06-22 23:55:26 +0500

    fix(JsonForm): Fixed layout issues

--- bluebase/storybook/configs/webpack.config.js
+++ bluebase/storybook/configs/webpack.config.js
@@ -1,24 +1,21 @@
 const { storybookWebpackConfigs } = require('@bluebase/cli-essentials');
 
-module.exports = (foo, bar, config) => {
-
+module.exports = ({ config }) => {
 	config = storybookWebpackConfigs({ config });
 
 	config.module.rules.push({
-    test: /\.js$/,
-    exclude: /node_modules[/\\](?!react-native-paper|react-native-vector-icons|react-native-safe-area-view)/,
-    use: {
-      loader: "babel-loader",
-      options: {
-        // Disable reading babel configuration
-        babelrc: false,
-        configFile: false,
+		test: /\.js$/,
+		exclude: /node_modules[/\\](?!react-native-paper|react-native-vector-icons|react-native-safe-area-view|@bluebase\/plugin-vector-icons\/node_modules\/react-native-vector-icons)/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				// Disable reading babel configuration
+				babelrc: false,
+				configFile: false,
 
-        presets: [
-					"@bluebase/code-standards/babel.config"
-				]
-      }
-		}
+				presets: ['@bluebase/code-standards/babel.config'],
+			},
+		},
 	});
 
 	return config;
