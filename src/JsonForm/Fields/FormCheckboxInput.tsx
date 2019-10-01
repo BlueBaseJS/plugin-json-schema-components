9f0775f80073b600ac401ed1769762ed384dcfe1
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-10-01 22:57:37 +0500

    fix(Switch & Checkbox): Removes extra padding when used with material-ui
    Closes #33

--- src/JsonForm/Fields/FormCheckboxInput.tsx
+++ src/JsonForm/Fields/FormCheckboxInput.tsx
@@ -1,9 +1,9 @@
+import { BlueBase, BlueBaseConsumer, getComponent } from '@bluebase/core';
 import { Checkbox, CheckboxProps } from '@bluebase/components';
 import { Field, getIn } from 'formik';
 
 import { BaseFormFieldProps } from '../BaseFormField';
 import React from 'react';
-import { getComponent } from '@bluebase/core';
 
 export interface FormCheckboxInputProps extends CheckboxProps {
 	type: 'checkbox';
@@ -14,20 +14,37 @@ const BaseFormField = getComponent<BaseFormFieldProps>('BaseFormField');
 
 export const FormCheckboxInput = (props: FormCheckboxInputProps) => {
 	return (
-		<Field {...props}>
-			{({ field, form }: any) => {
-				const name = props.name;
-				const value = getIn(form.values, name);
+		<BlueBaseConsumer>
+			{(BB: BlueBase) => (
+				<Field {...props}>
+					{({ field, form }: any) => {
+						const name = props.name;
+						const value = getIn(form.values, name);
 
-				const inputProps = {
-					...props,
-					checked: Boolean(value),
-					onValueChange: (_value: any, checked: boolean) => form.setFieldValue(field.name, checked),
-				};
+						const inputProps = {
+							...props,
+							checked: Boolean(value),
+							onValueChange: (_value: any, checked: boolean) =>
+								form.setFieldValue(field.name, checked),
+						};
 
-				return <BaseFormField MainComponent={Checkbox} {...inputProps} />;
-			}}
-		</Field>
+						// Dirty Hack: We have to reduce vertical padding because
+						// MUI checkboxes have their own padding
+						const styles: any = {};
+						const source = BB.Components.getMeta('Checkbox', 'source', {});
+
+						if (source.type === 'plugin' && source.key === 'material-ui') {
+							styles.root = {
+								paddingVertical: 4,
+							};
+						}
+						// End of Dirty Hack
+
+						return <BaseFormField MainComponent={Checkbox} styles={styles} {...inputProps} />;
+					}}
+				</Field>
+			)}
+		</BlueBaseConsumer>
 	);
 };
 
