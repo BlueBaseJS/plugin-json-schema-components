2fdc388f599b7b8081a867b7856c98144e8f1e26
Abdul Rehman Talat <rehman.talat@gmail.com>
2020-10-12 19:08:36 +0500

    chore: Updated dependencies

--- src/JsonGraphqlForm/__stories__/JsonGraphqlForm.stories.tsx
+++ src/JsonGraphqlForm/__stories__/JsonGraphqlForm.stories.tsx
@@ -1,6 +1,6 @@
 import { Card } from '@bluebase/components';
 import { EditProfileProfileForm } from './EditProfileProfileForm';
-import { MockedProvider } from 'react-apollo/test-utils';
+import { MockedProvider } from '@apollo/react-testing';
 import React from 'react';
 import { SignupForm } from './SignupForm';
 import { mocks } from '.';
@@ -11,41 +11,41 @@ const onSuccess = (_res: any, _vals: any, { setStatus }: any) => setStatus({ suc
 storiesOf('JsonGraphqlForm', module)
 
 
-.add('With Network Error', () => (
-	<Card style={{ width: 400, margin: 40 }}>
-		<SignupForm />
-	</Card>
-))
-
-.add('With Authentication Error', () => (
-	<Card style={{ width: 400, margin: 40 }}>
-		<MockedProvider mocks={[mocks.graphQLErrorsUnAuthenticated]} addTypename={false}>
+	.add('With Network Error', () => (
+		<Card style={{ width: 400, margin: 40 }}>
 			<SignupForm />
-		</MockedProvider>
-	</Card>
-))
+		</Card>
+	))
 
-.add('With Bad User Input', () => (
-	<Card style={{ width: 400, margin: 40 }}>
-		<MockedProvider mocks={[mocks.graphQLErrorsBadUserInput]} addTypename={false}>
-			<SignupForm />
-		</MockedProvider>
-	</Card>
-))
+	.add('With Authentication Error', () => (
+		<Card style={{ width: 400, margin: 40 }}>
+			<MockedProvider mocks={[mocks.graphQLErrorsUnAuthenticated]} addTypename={false}>
+				<SignupForm />
+			</MockedProvider>
+		</Card>
+	))
 
-.add('Success', () => (
-	<Card style={{ width: 400, margin: 40 }}>
-		<MockedProvider mocks={[mocks.success]} addTypename={false}>
-			<SignupForm onSuccess={onSuccess} />
-		</MockedProvider>
-	</Card>
-))
+	.add('With Bad User Input', () => (
+		<Card style={{ width: 400, margin: 40 }}>
+			<MockedProvider mocks={[mocks.graphQLErrorsBadUserInput]} addTypename={false}>
+				<SignupForm />
+			</MockedProvider>
+		</Card>
+	))
 
-.add('Initial Data from Query', () => (
-	<Card style={{ width: 400, margin: 40 }}>
-		<MockedProvider mocks={[mocks.viewerQuery]} addTypename={false}>
-			<EditProfileProfileForm />
-		</MockedProvider>
-	</Card>
-))
+	.add('Success', () => (
+		<Card style={{ width: 400, margin: 40 }}>
+			<MockedProvider mocks={[mocks.success]} addTypename={false}>
+				<SignupForm onSuccess={onSuccess} />
+			</MockedProvider>
+		</Card>
+	))
+
+	.add('Initial Data from Query', () => (
+		<Card style={{ width: 400, margin: 40 }}>
+			<MockedProvider mocks={[mocks.viewerQuery]} addTypename={false}>
+				<EditProfileProfileForm />
+			</MockedProvider>
+		</Card>
+	))
 ;
